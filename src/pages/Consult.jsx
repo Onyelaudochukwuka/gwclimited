c24d56ab0c8b163219e51a12515e569c72a93943
Messiovo <messiovo@gmail.com>
2022-06-30 13:27:36 +0100

    changes

--- src/pages/Consult.jsx
+++ src/pages/Consult.jsx
@@ -6,7 +6,7 @@ const Consult = () => {
     <div className='p-10 rounded-lg lg:mx-10 h-auto mb-20'>
         <h2 className='text-[#252F46] font-bold text-xl lg:text-2xl' >Consultancy</h2>
         <div className="w-20 h-2 bg-[#C2956D] rounded-full div-animate"></div>
-        <img src={consultpic} alt="" className=' h-[30rem] m-auto w-3/5 py-10'/>
+        <img src={consultpic} alt="" className=' h-auto m-auto w-3/5 py-10'/>
         <div>
             <p className='text-xs lg:text-xl py-8 leading-normal'>At Globalwatch Catalogue we provide expert advice on matters concerning various fields such as travel, business development, 
             human capacity building, education and multimedia application.</p>
--- src/pages/StudyAbroad.jsx
+++ src/pages/StudyAbroad.jsx
@@ -42,7 +42,7 @@ const StudyAbroad = () => {
     <div className='p-10 rounded-lg lg:mx-10 h-auto mb-20'>
         <h2 className='text-[#252F46] font-bold text-xl lg:text-2xl' >Study Abroad</h2>
         <div className="w-20 h-2 bg-[#C2956D] rounded-full div-animate"></div>
-        <img src={picture} alt="" className=' h-[30rem] m-auto w-3/5 py-10'/>
+        <img src={picture} alt="" className=' h-auto m-auto w-3/5 py-10'/>
         <div>
             <p className='text-xs lg:text-xl py-8 leading-normal'>
                 We provide overseas students with higher education opportunities across the 
